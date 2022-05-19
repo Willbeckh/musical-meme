674b051df80b0a84ab9d1ac7bb18d4f3729f7e22
Willbeckh <wileyhndirangu04@gmail.com>
2022-05-19 09:51:48 +0300

    create application configs

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
-from distutils.debug import DEBUG
 import os
 
 
 class Config:
     """This class sets the main application configuration settings"""
-    pass
-
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SECRET_KEY=os.environ.get('SECRET_KEY') or '<insert_secret_key_here>'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevConfig(Config):
     """Development configuration."""
