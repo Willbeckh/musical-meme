9806edd3cbd50434a7a173631338710a9334e269
Willbeckh <wileyhndirangu04@gmail.com>
2022-05-19 18:58:03 +0300

    add post link

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 import requests
 import random
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, abort
 from flask_login import login_required, current_user
 from app.main.links import main
 from app.models import *
@@ -31,3 +31,37 @@ def add_post():
         flash('Your post has been created!' , 'info')
         return redirect(url_for('main.index'))
     return render_template('add_post.html', title="Add post", form=form)
+
+
+@main.route('/post/<int:post_id>')
+# @login_required
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    comments = Comment.query.all()
+    return render_template('post.html', title=post.title, post=post,  comments=comments)
+
+
+@main.route('/post/<int:post_id>/comment', methods=['GET', 'POST'])
+# @login_required
+def comment(post_id):
+    """Method to enable a user to comment on a post"""
+    form = CommentForm()
+    if form.validate_on_submit():
+        comment = Comment(comment_text=form.comment_text.data,
+                          author=current_user, post_id=post_id)
+        db.session.add(comment)
+        db.session.commit()
+        return redirect(url_for('main.post', post_id=post_id))
+    return render_template('comment.html', title="Comment", form=form)
+
+
+@main.route('/post/<int:post_id>/delete', methods=['POST'])
+@login_required
+def delete_post(post_id):
+  post = Post.query.get_or_404(post_id)
+  if post.author != current_user:
+    abort(403)
+  db.session.delete(post)
+  db.session.commit()
+  flash(f'Your post `Title: {post.title}` has been Deleted!', 'info')
+  return redirect(url_for('main.index'))
