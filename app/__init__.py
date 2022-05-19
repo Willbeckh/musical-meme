9fe3ea8de34eb484de1b1a93aba11c43072b6544
Willbeckh <wileyhndirangu04@gmail.com>
2022-05-19 09:18:29 +0300

    add login manager extensions

--- app/__init__.py
+++ app/__init__.py
@@ -2,21 +2,30 @@
 from flask import Flask
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+
 
 db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
     """method that sets the main application factory"""
-    app = Flask(__name__)
+    app = Flask(__name__, instance_relative_config=True)
     
-    # app bluprints
+     # app configs
+    app.config.from_object(config_options[config_name])
+        
+     # init flask ext
+    db.init_app(app)
+    migrate = Migrate(app, db)
+    login_manager.init_app(app)
+
+    
+    # app blueprints
     from .main import main as main_bp
     app.register_blueprint(main_bp)
-
-    # init flask ext
-    db.init_app(app)
     
-    # app configs
-    app.config.from_object(config_options[config_name])
-    
+   
     return app
