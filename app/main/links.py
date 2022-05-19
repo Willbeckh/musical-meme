616bd7089035c717f05bd1b09dabf40b086a3e00
Willbeckh <wileyhndirangu04@gmail.com>
2022-05-19 09:52:32 +0300

    create main blueprints location

---
+++ app/main/links.py
@@ -0,0 +1,8 @@
+'''all application blueprints'''
+from flask import Blueprint
+
+main = Blueprint('main', __name__)
+auth = Blueprint('auth', __name__, url_prefix='/authenticate')
+
+from app.main import views
+from app.auth import views
