d504f7a0b1b67a88ed41c8e577846e6f40c302c9
Willbeckh <wileyhndirangu04@gmail.com>
2022-05-19 09:53:59 +0300

    create db query for posts

--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,9 @@
-from . import main
 from flask import render_template, abort, redirect, url_for, request, flash, session
-from jinja2 import TemplateNotFound
+from app.main.links import main
+from app.models import *
+
 
 @main.route('/')
 def index():
-    try:
-        return render_template('index.html')
-    except TemplateNotFound:
-        abort(404)
+    posts = Post.query.all()
+    return render_template('index.html', posts=posts)
