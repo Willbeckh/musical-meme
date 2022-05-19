0bb02520bf2d883dec3755e487d93d9f1445467e
Willbeckh <wileyhndirangu04@gmail.com>
2022-05-19 10:05:26 +0300

    create logic for logging a user.

---
+++ app/auth/views.py
@@ -0,0 +1,44 @@
+'''login route'''
+from flask import render_template, redirect, url_for, flash, request
+from flask_login import current_user, login_user, logout_user
+from app.models import User
+from app.auth.forms import LoginForm
+from app.main.links import auth
+from app import db
+
+
+# login route
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    '''
+    Function that creates the login logic.
+    '''
+    if current_user.is_authenticated:
+        return redirect(url_for('main.index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash("Invalid username or password.", 'error')
+            return redirect(url_for('auth.login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        return redirect(next_page) if next_page else redirect(url_for('main.index'))
+    return render_template('authenticate/login.html', title='Sign In', form=form)
+
+
+@auth.route('/logout')
+def logout():
+    logout_user()
+    flash('You have been logged out.', 'success')
+    return redirect(url_for('main.index'))
+
+
+# register route
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    '''
+    user sign up route method
+    '''
+    pass
+
