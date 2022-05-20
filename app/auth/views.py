847476cd3a36bcad8b144c2011cb6826a17e01cd
Willbeckh <wileyhndirangu04@gmail.com>
2022-05-20 14:35:33 +0300

    pt: update logine view and form r=to use email

--- app/auth/views.py
+++ app/auth/views.py
@@ -17,7 +17,7 @@ def login():
         return redirect(url_for('main.index'))
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
+        user = User.query.filter_by(email=form.email.data).first()
         if user is None or not user.check_password(form.password.data):
             flash("Invalid username or password.", 'danger')
             return redirect(url_for('auth.login'))
